7cd27efc7a1f689cdae93905b351047777835f4e
Iwo Plaza <gobbobpl@gmail.com>
2016-09-05 20:07:37 +0200

    Moved world generation to the World class.

--- js/world/chunk.js
+++ js/world/chunk.js
@@ -1,88 +1,16 @@
-function Chunk(w_type){
+function Chunk(p_x, p_z){
 	this.layers = new Array(0);
-	this.generateTerrain = function(_type){
-        this.layers = new Array(0);
-		if (_type == "FLAT"){
-            //FlatWorld xD
-            this.layers[0] = new Array(Chunk.width);
-            for (var x = 0; x < Chunk.width; x++){
-                this.layers[0][x] = new Array(Chunk.width);
-                for (var z = 0; z < Chunk.width; z++){
-                    this.layers[0][x][z] = {id:11};
-                }
-            }
-            for (var y = 1; y < 5; y++){
-                this.layers[y] = new Array(Chunk.width);
-                for (var x = 0; x < Chunk.width; x++){
-                    this.layers[y][x] = new Array(Chunk.width);
-                    for (var z = 0; z < Chunk.width; z++){
-                        this.layers[y][x][z] = {id:2};
-                    }
-                }
-            }
-            this.layers[5] = new Array(Chunk.width);
-            for (var x = 0; x < Chunk.width; x++){
-                this.layers[5][x] = new Array(Chunk.width);
-                for (var z = 0; z < Chunk.width; z++){
-                    this.layers[5][x][z] = {id:3};
-                }
-            }
-        }else
-        if (_type == "DEBUG"){
-            this.layers[0] = new Array(Chunk.width);
-            for (var x=0; x<Chunk.width;x++){
-                this.layers[0][x] = new Array(Chunk.width);
-                for (var z = 0; z < Chunk.width; z++){
-                    if (1+z+(x*8)<Blocks.blocks.length){
-                        this.layers[0][x][z] = {id: (1+z+(x*8))};
-                    }else{
-                        this.layers[0][x][z] = {id:0};
-                    }
-                }
-            }
-        }
-        /*
-            
-        //Tree code (Needs rework)
-            
-            this.layers[12][4][4] = {id:14};
-            this.layers[12][3][4] = {id:14};
-            this.layers[12][4][3] = {id:14};
-            this.layers[12][5][4] = {id:14};
-            this.layers[12][4][5] = {id:14};
-            for (var x=0;x<3;x++){
-                for (var z=0;z<3;z++){
-                    this.layers[11][3+x][3+z] = {id:14};
-                }
-            }
-            for (var x=0;x<5;x++){
-                for (var z=0;z<5;z++){
-                    this.layers[10][2+x][2+z] = {id:14};
-                }
-            }
-            for (var x=0;x<3;x++){
-                for (var z=0;z<3;z++){
-                    this.layers[9][3+x][3+z] = {id:14};
-                }
-            }
-            this.layers[9][3][2] = {id:14};
-            this.layers[9][4][2] = {id:14};
-            this.layers[9][5][2] = {id:14};
-            this.layers[9][2][3] = {id:14};
-            this.layers[9][2][4] = {id:14};
-            this.layers[9][2][5] = {id:14};
-            this.layers[9][6][5] = {id:14};
-            this.layers[9][6][4] = {id:14};
-            this.layers[9][6][3] = {id:14};
-            this.layers[9][5][6] = {id:14};
-            this.layers[9][4][6] = {id:14};
-            this.layers[9][3][6] = {id:14};
-            for (var i=0;i<6;i++) this.layers[6+i][4][4] = {id:13};
-        */
-        
-	}
+    var chunkX = p_x;
+    var chunkZ = p_z;
     
-    this.generateTerrain(w_type);
+    this.getX = function() {
+        return chunkX;
+    }
+    
+    this.getZ = function() {
+        return chunkZ;
+    }
 }
+
 Chunk.width = 8;
 Chunk.height = 256;
--- js/world/world.js
+++ js/world/world.js
@@ -1,13 +1,20 @@
 function World(p_name,p_type){
+    //Private variables
+    var type = p_type;
+    
+    //Public variables
 	this.name = p_name;
-    this.type = p_type;
 	this.chunks = new Array(0);
     this.blockMesh = undefined;
     
 	this.getName = function() {
 		return this.name;
 	}
-
+    
+    this.getType = function() {
+        return type;
+    }
+    
 	this.renderBlocks = function() {
         var chunkKeys = Object.keys(this.chunks);
         console.dir(chunkKeys);
@@ -49,5 +56,92 @@ function World(p_name,p_type){
         this.chunks[""+p_x+"x"+p_z] = p_chunk;
     }
     
-    this.setChunk(0, 0, new Chunk(this.type));
+    this.generateChunk = function(p_x, p_z) {
+        var chunk = this.getChunk(p_x, p_z);
+        
+        chunk.layers = new Array(0);
+        
+        if (this.getType() == "FLAT"){
+            chunk.layers[0] = new Array(Chunk.width);
+            for (var x = 0; x < Chunk.width; x++){
+                chunk.layers[0][x] = new Array(Chunk.width);
+                for (var z = 0; z < Chunk.width; z++){
+                    chunk.layers[0][x][z] = {id:11};
+                }
+            }
+            for (var y = 1; y < 5; y++){
+                chunk.layers[y] = new Array(Chunk.width);
+                for (var x = 0; x < Chunk.width; x++){
+                    chunk.layers[y][x] = new Array(Chunk.width);
+                    for (var z = 0; z < Chunk.width; z++){
+                        chunk.layers[y][x][z] = {id:2};
+                    }
+                }
+            }
+            chunk.layers[5] = new Array(Chunk.width);
+            for (var x = 0; x < Chunk.width; x++){
+                chunk.layers[5][x] = new Array(Chunk.width);
+                for (var z = 0; z < Chunk.width; z++){
+                    chunk.layers[5][x][z] = {id:3};
+                }
+            }
+        }else if (this.getType() == "DEBUG"){
+            chunk.layers[0] = new Array(Chunk.width);
+            for (var x=0; x<Chunk.width;x++){
+                chunk.layers[0][x] = new Array(Chunk.width);
+                for (var z = 0; z < Chunk.width; z++){
+                    if (1+z+(x*8)<Blocks.blocks.length){
+                        chunk.layers[0][x][z] = {id: (1+z+(x*8))};
+                    }else{
+                        chunk.layers[0][x][z] = {id:0};
+                    }
+                }
+            }
+        }
+        /*
+            
+        //Tree code (Needs rework)
+            
+            this.layers[12][4][4] = {id:14};
+            this.layers[12][3][4] = {id:14};
+            this.layers[12][4][3] = {id:14};
+            this.layers[12][5][4] = {id:14};
+            this.layers[12][4][5] = {id:14};
+            for (var x=0;x<3;x++){
+                for (var z=0;z<3;z++){
+                    this.layers[11][3+x][3+z] = {id:14};
+                }
+            }
+            for (var x=0;x<5;x++){
+                for (var z=0;z<5;z++){
+                    this.layers[10][2+x][2+z] = {id:14};
+                }
+            }
+            for (var x=0;x<3;x++){
+                for (var z=0;z<3;z++){
+                    this.layers[9][3+x][3+z] = {id:14};
+                }
+            }
+            this.layers[9][3][2] = {id:14};
+            this.layers[9][4][2] = {id:14};
+            this.layers[9][5][2] = {id:14};
+            this.layers[9][2][3] = {id:14};
+            this.layers[9][2][4] = {id:14};
+            this.layers[9][2][5] = {id:14};
+            this.layers[9][6][5] = {id:14};
+            this.layers[9][6][4] = {id:14};
+            this.layers[9][6][3] = {id:14};
+            this.layers[9][5][6] = {id:14};
+            this.layers[9][4][6] = {id:14};
+            this.layers[9][3][6] = {id:14};
+            for (var i=0;i<6;i++) this.layers[6+i][4][4] = {id:13};
+        */
+    }
+    
+    this.generateTerrain = function(){
+        this.setChunk(0, 0, new Chunk(0, 0));
+        this.generateChunk(0, 0);
+	}
+    
+    this.generateTerrain();
 }
